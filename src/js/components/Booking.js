697472e0a68277e418eaf2eae60bf166fd6e7fe1
Maciej Pomaranski <maciej.pomaranski@gmail.com>
2023-07-01 10:51:56 +0200

    Add new wigets still one broke

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -23,6 +23,8 @@ class Booking{
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
 
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     thisBooking.dom.wrapper = generatedHTML;
 
   }
